7ad2d8439bb3982e50b7c702fa79e1eaaba7bb85
Jiahe Jiang <61075605+Isrothy@users.noreply.github.com>
2024-12-28 15:51:06 -0600

    fix: barbar

--- lua/nordify/groups/barbar.lua
+++ lua/nordify/groups/barbar.lua
@@ -7,9 +7,9 @@ M.url = "https://github.com/romgrk/barbar.nvim"
 ---@type Nordify.HighlightsFn
 function M.get(c, config)
     local current_hi = vim.tbl_extend("force", { bg = c.polar_night.brighter }, config.styles.bufferline.current)
-    local global_bg = utils.make_global_bg()
+    local bg = c.polar_night.origin
     return {
-        BufferTabpageFill = { bg = global_bg, fg = c.polar_night.origin },
+        BufferTabpageFill = { bg = bg, fg = c.polar_night.origin },
         BufferCurrent = current_hi,
         BufferCurrentIndex = current_hi,
         BufferCurrentMod = vim.tbl_extend(
@@ -40,21 +40,21 @@ function M.get(c, config)
         BufferVisibleINFO = { bg = c.polar_night.bright, fg = c.frost.ice },
         BufferVisibleWARN = { bg = c.polar_night.bright, fg = c.aurora.yellow },
         BufferVisibleERROR = { bg = c.polar_night.bright, fg = c.aurora.red },
-        BufferInactive = { bg = global_bg, fg = c.polar_night.light },
-        BufferInactiveIndex = { bg = global_bg, fg = c.polar_night.light },
+        BufferInactive = { bg = bg, fg = c.polar_night.light },
+        BufferInactiveIndex = { bg = bg, fg = c.polar_night.light },
         BufferInactiveMod = vim.tbl_extend(
             "force",
-            { bg = global_bg, fg = utils.darken(c, c.aurora.yellow, 0.4) },
+            { bg = bg, fg = utils.darken(c, c.aurora.yellow, 0.4) },
             config.styles.bufferline.modified
         ),
-        BufferInactiveSign = { bg = global_bg, fg = c.polar_night.origin },
-        BufferInactiveTarget = { bg = global_bg, fg = c.aurora.red, bold = true },
-        BufferInactiveHINT = { bg = global_bg, fg = utils.darken(c, c.frost.artic_water, 0.4) },
-        BufferInactiveINFO = { bg = global_bg, fg = utils.darken(c, c.frost.ice, 0.4) },
-        BufferInactiveWARN = { bg = global_bg, fg = utils.darken(c, c.aurora.yellow, 0.4) },
-        BufferInactiveERROR = { bg = global_bg, fg = utils.darken(c, c.aurora.red, 0.4) },
-        BufferTabpages = { bg = global_bg, fg = c.none },
-        BufferTabpage = { bg = global_bg },
+        BufferInactiveSign = { bg = bg, fg = c.polar_night.origin },
+        BufferInactiveTarget = { bg = bg, fg = c.aurora.red, bold = true },
+        BufferInactiveHINT = { bg = bg, fg = utils.darken(c, c.frost.artic_water, 0.4) },
+        BufferInactiveINFO = { bg = bg, fg = utils.darken(c, c.frost.ice, 0.4) },
+        BufferInactiveWARN = { bg = bg, fg = utils.darken(c, c.aurora.yellow, 0.4) },
+        BufferInactiveERROR = { bg = bg, fg = utils.darken(c, c.aurora.red, 0.4) },
+        BufferTabpages = { bg = bg, fg = c.none },
+        BufferTabpage = { bg = bg },
     }
 end
 
